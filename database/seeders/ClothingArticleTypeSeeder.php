6334c912d554540da75d878d7b5a0d137a5b2063
Alexander Faus <alexfaus08@gmail.com>
2022-03-03 14:10:07 -0500

    created ClothingArticleTypeSeeder

--- database/seeders/ClothingArticleTypeSeeder.php
+++ database/seeders/ClothingArticleTypeSeeder.php
@@ -2,12 +2,24 @@
 
 namespace Database\Seeders;
 
+use App\Models\ClothingArticleType;
 use Illuminate\Database\Seeder;
 
 class ClothingArticleTypeSeeder extends Seeder
 {
     public function run()
     {
-        //
+        ClothingArticleType::create(
+          ['name' => 'top']
+        );
+        ClothingArticleType::create(
+          ['name' => 'bottom']
+        );
+        ClothingArticleType::create(
+            ['name' => 'shoes']
+        );
+        ClothingArticleType::create(
+            ['name' => 'fullbody']
+        );
     }
 }

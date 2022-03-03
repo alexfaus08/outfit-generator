9985439837d7ac8afd913bc850a90818ab66d239
Alexander Faus <alexfaus08@gmail.com>
2022-03-03 14:34:21 -0500

    changed seeders to use variable ids

--- database/seeders/ClothingArticleSeeder.php
+++ database/seeders/ClothingArticleSeeder.php
@@ -3,46 +3,59 @@
 namespace Database\Seeders;
 
 use App\Models\ClothingArticle;
+use App\Models\ClothingArticleType;
 use Illuminate\Database\Seeder;
 
 class ClothingArticleSeeder extends Seeder
 {
     public function run()
     {
+        $top = ClothingArticleType::create(
+            ['name' => 'top']
+        );
+        $bottom = ClothingArticleType::create(
+            ['name' => 'bottom']
+        );
+        $shoes = ClothingArticleType::create(
+            ['name' => 'shoes']
+        );
+        $fullbody = ClothingArticleType::create(
+            ['name' => 'fullbody']
+        );
         ClothingArticle::create(
-            ['image_path' => '/storage/app/images/tops/yellow-t-shirt.png', 'clothing_article_type_id' => 1,
+            ['image_path' => '/storage/app/images/tops/yellow-t-shirt.png', 'clothing_article_type_id' => $top->id,
             'user_id' => 1]
         );
         ClothingArticle::create(
-            ['image_path' => '/storage/app/images/tops/blue-button-up.png', 'clothing_article_type_id' => 1,
+            ['image_path' => '/storage/app/images/tops/blue-button-up.png', 'clothing_article_type_id' => $top->id,
                 'user_id' => 1]
         );
         ClothingArticle::create(
-            ['image_path' => '/storage/app/images/tops/orange-button-up.png', 'clothing_article_type_id' => 1,
+            ['image_path' => '/storage/app/images/tops/orange-button-up.png', 'clothing_article_type_id' => $top->id,
                 'user_id' => 1]
         );
         ClothingArticle::create(
-            ['image_path' => '/storage/app/images/bottoms/chinos.png', 'clothing_article_type_id' => 2,
+            ['image_path' => '/storage/app/images/bottoms/chinos.png', 'clothing_article_type_id' => $bottom->id,
                 'user_id' => 1]
         );
         ClothingArticle::create(
-            ['image_path' => '/storage/app/images/bottoms/khaki.png', 'clothing_article_type_id' => 2,
+            ['image_path' => '/storage/app/images/bottoms/khaki.png', 'clothing_article_type_id' => $bottom->id,
                 'user_id' => 1]
         );
         ClothingArticle::create(
-            ['image_path' => '/storage/app/images/shoes/boots.png', 'clothing_article_type_id' => 3,
+            ['image_path' => '/storage/app/images/shoes/boots.png', 'clothing_article_type_id' => $shoes->id,
                 'user_id' => 1]
         );
         ClothingArticle::create(
-            ['image_path' => '/storage/app/images/shoes/oxfords.png', 'clothing_article_type_id' => 3,
+            ['image_path' => '/storage/app/images/shoes/oxfords.png', 'clothing_article_type_id' => $shoes->id,
                 'user_id' => 1]
         );
         ClothingArticle::create(
-            ['image_path' => '/storage/app/images/shoes/sneakers.png', 'clothing_article_type_id' => 3,
+            ['image_path' => '/storage/app/images/shoes/sneakers.png', 'clothing_article_type_id' => $shoes->id,
                 'user_id' => 1]
         );
         ClothingArticle::create(
-            ['image_path' => '/storage/app/images/full/overalls.png', 'clothing_article_type_id' => 4,
+            ['image_path' => '/storage/app/images/full/overalls.png', 'clothing_article_type_id' => $fullbody->id,
                 'user_id' => 1]
         );
     }

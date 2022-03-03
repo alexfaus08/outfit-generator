81c8f1e2177c4274d17475c92599f090739ec0cd
Alexander Faus <alexfaus08@gmail.com>
2022-03-03 14:30:36 -0500

    Seeding clothing articles

--- database/seeders/ClothingArticleSeeder.php
+++ database/seeders/ClothingArticleSeeder.php
@@ -9,6 +9,41 @@ class ClothingArticleSeeder extends Seeder
 {
     public function run()
     {
-        //
+        ClothingArticle::create(
+            ['image_path' => '/storage/app/images/tops/yellow-t-shirt.png', 'clothing_article_type_id' => 1,
+            'user_id' => 1]
+        );
+        ClothingArticle::create(
+            ['image_path' => '/storage/app/images/tops/blue-button-up.png', 'clothing_article_type_id' => 1,
+                'user_id' => 1]
+        );
+        ClothingArticle::create(
+            ['image_path' => '/storage/app/images/tops/orange-button-up.png', 'clothing_article_type_id' => 1,
+                'user_id' => 1]
+        );
+        ClothingArticle::create(
+            ['image_path' => '/storage/app/images/bottoms/chinos.png', 'clothing_article_type_id' => 2,
+                'user_id' => 1]
+        );
+        ClothingArticle::create(
+            ['image_path' => '/storage/app/images/bottoms/khaki.png', 'clothing_article_type_id' => 2,
+                'user_id' => 1]
+        );
+        ClothingArticle::create(
+            ['image_path' => '/storage/app/images/shoes/boots.png', 'clothing_article_type_id' => 3,
+                'user_id' => 1]
+        );
+        ClothingArticle::create(
+            ['image_path' => '/storage/app/images/shoes/oxfords.png', 'clothing_article_type_id' => 3,
+                'user_id' => 1]
+        );
+        ClothingArticle::create(
+            ['image_path' => '/storage/app/images/shoes/sneakers.png', 'clothing_article_type_id' => 3,
+                'user_id' => 1]
+        );
+        ClothingArticle::create(
+            ['image_path' => '/storage/app/images/full/overalls.png', 'clothing_article_type_id' => 4,
+                'user_id' => 1]
+        );
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\ClothingArticleType;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::factory()->count(2)->create();
         $this->call(ClothingArticleTypeSeeder::class);
+        $this->call(ClothingArticleSeeder::class);
     }
 }

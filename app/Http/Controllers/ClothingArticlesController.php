b9010faa9813a0a5522f74de0ab53a5794326452
Alexander Faus <alexfaus08@gmail.com>
2022-03-03 13:42:01 -0500

    create ClothingArticle Model, Seeder, and Controller

---
+++ app/Http/Controllers/ClothingArticlesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClothingArticle;
+use Illuminate\Http\Request;
+
+class ClothingArticlesController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(ClothingArticle $clothingArticle)
+    {
+        //
+    }
+
+    public function edit(ClothingArticle $clothingArticle)
+    {
+        //
+    }
+
+    public function update(Request $request, ClothingArticle $clothingArticle)
+    {
+        //
+    }
+
+    public function destroy(ClothingArticle $clothingArticle)
+    {
+        //
+    }
+}

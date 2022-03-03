4f67247c69b61521abf4406a039ae0dec4fc8dbb
Alexander Faus <alexfaus08@gmail.com>
2022-03-03 13:42:55 -0500

    create ClothingArticleType model, seeder and controller

---
+++ app/Http/Controllers/ClothingArticleTypesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClothingArticleType;
+use Illuminate\Http\Request;
+
+class ClothingArticleTypesController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(ClothingArticleType $clothingArticleType)
+    {
+        //
+    }
+
+    public function edit(ClothingArticleType $clothingArticleType)
+    {
+        //
+    }
+
+    public function update(Request $request, ClothingArticleType $clothingArticleType)
+    {
+        //
+    }
+
+    public function destroy(ClothingArticleType $clothingArticleType)
+    {
+        //
+    }
+}

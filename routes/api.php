a7ae169ca603582e214a7013f3a8746d1f1bf709
Alexander Faus <alexfaus08@gmail.com>
2022-03-04 10:00:41 -0500

    change route wording

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,7 @@ Route::get('/article-types', [ClothingArticleTypeController::class, 'index']);
 
 // clothing article type top routes
 Route::get('/tops', [ClothingArticleTopController::class, 'index']);
-Route::get('/rantop', [ClothingArticleTopController::class, 'show']);
+Route::get('/random-top', [ClothingArticleTopController::class, 'show']);
 
 // clothing article type bottom routes
 Route::get('/bottoms', [ClothingArticleBottomController::class, 'index']);

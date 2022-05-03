cf19de577cff6998ee4e02bfd51324a5a652877c
Alexander Faus <alexfaus08@gmail.com>
2022-05-03 18:26:23 -0400

    added buefy

--- resources/js/views/GeneratorShow.vue
+++ resources/js/views/GeneratorShow.vue
@@ -1,14 +1,28 @@
 <template>
-    <div>
-        
+    <div class="columns is-centered is-mobile">
+        <div class="column is-half has-text-centered">
+            <ClothingArticle />
+            <ClothingArticle />
+            <ClothingArticle />
+            <b-button type="is-primary" >
+                Test
+            </b-button>
+        </div>
     </div>
 </template>
 
 <script>
 import ClothingArticle from "../components/ClothingArticle";
+import axios from 'axios';
 
 export default {
-    name: "GeneratorShow"
+    name: "GeneratorShow",
+    components: {ClothingArticle},
+    mounted() {
+        axios.get('/api/clothing_article').then((res) => {
+            console.log(res);
+        });
+    }
 }
 </script>
 

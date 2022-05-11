762eda5487be193181c43f414fe05b74bb886c14
Alexander Faus <alexfaus08@gmail.com>
2022-05-11 17:18:42 -0400

    use function

--- database/seeders/ClothingArticleSeeder.php
+++ database/seeders/ClothingArticleSeeder.php
@@ -14,64 +14,15 @@ class ClothingArticleSeeder extends Seeder
     {
         $fileExtension = 'png';
         $rootFolderPath = '/Users/alexander/Desktop/Clothes';
-        // tops folder
-        $topsFolder = $rootFolderPath . '/tops/';
-        $topsImages = scandir($topsFolder);
-        foreach($topsImages as $topimg) {
-            if (pathinfo($topimg)['extension'] === $fileExtension) {
-                $data = [
-                    'image_path' => $topsFolder . $topimg,
-                    'clothing_article_type' => 'top',
-                    'user_id' => 1
-                ];
-                ClothingArticleService::store($data);
-            }
-        }
-        // bottoms folder
-        $bottomsfolder = $rootFolderPath . '/bottoms/';
-        $bottomsImages = scandir($bottomsfolder);
-        foreach($bottomsImages as $bottomimg) {
-            if (pathinfo($bottomimg)['extension'] === $fileExtension) {
-                $data = [
-                    'image_path' => $bottomsfolder . $bottomimg,
-                    'clothing_article_type' => 'bottom',
-                    'user_id' => 1
-                ];
-                ClothingArticleService::store($data);
-            }
-        }
-        // shoes folder
-        $shoesfolder = $rootFolderPath . '/shoes/';
-        $shoesImages = scandir($shoesfolder);
-        foreach($shoesImages as $shoeimg) {
-            if (pathinfo($shoeimg)['extension'] === $fileExtension) {
-                $data = [
-                    'image_path' => $shoesfolder . $shoeimg,
-                    'clothing_article_type' => 'shoes',
-                    'user_id' => 1
-                ];
-                ClothingArticleService::store($data);
-            }
-        }
-        // fullbody folder
-        $fullbodyfolder = $rootFolderPath . '/fullbody/';
-        $fullbodysImages = scandir($fullbodyfolder);
-        foreach($fullbodysImages as $fullbodyimg) {
-            if (pathinfo($fullbodyimg)['extension'] === $fileExtension) {
-                $data = [
-                    'image_path' => $fullbodyfolder . $fullbodyimg,
-                    'clothing_article_type' => 'fullbody',
-                    'user_id' => 1
-                ];
-                ClothingArticleService::store($data);
-            }
-        }
+        $this->populateClothingArticleType($rootFolderPath, 'tops', 'top', $fileExtension);
+        $this->populateClothingArticleType($rootFolderPath, 'bottoms', 'bottom', $fileExtension);
+        $this->populateClothingArticleType($rootFolderPath, 'fullbody', 'fullbody', $fileExtension);
+        $this->populateClothingArticleType($rootFolderPath, 'shoes', 'shoes', $fileExtension);
     }
 
-    public function populateClothingArticleType($rootFolderPath, $clothingType, $fileExtension) {
+    public function populateClothingArticleType($rootFolderPath, $clothingFolder, $clothingType, $fileExtension) {
 
-        // $fullpath = $rootFolderPath . '/fullbody/';
-        $fullpath = $rootFolderPath . '/' . $clothingType . '/';
+        $fullpath = $rootFolderPath . '/' . $clothingFolder . '/';
         $imgs = scandir($fullpath);
         foreach($imgs as $img) {
             if (pathinfo($img)['extension'] === $fileExtension) {
